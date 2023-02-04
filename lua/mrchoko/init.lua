e4a986769199bd6bd8ff84d4be8901e33b02c991
abhishek mallepalli <mallepalliabhishek@gmail.com>
2023-02-04 12:29:02 -0800

    added some sane defaults, set the backups and undo folder explicitly and wrote functions so that it creates them if they don't exist

--- lua/mrchoko/init.lua
+++ lua/mrchoko/init.lua
@@ -12,37 +12,53 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
---vim.opt.backup = false -- creates a backup file
 --vim.opt.colorcolumn = "99999" -- fixes indentline for now
 --vim.opt.completeopt = { "menuone", "noselect" }
 --vim.opt.foldmethod = "manual" -- folding set to "expr" for treesitter based folding
 --vim.opt.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
 --vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
---vim.opt.hidden = true -- required to keep multiple buffers and open multiple buffers
 --vim.opt.hlsearch = true -- highlight all matches on previous search pattern
---vim.opt.ignorecase = true -- ignore case in search patterns
---vim.opt.mouse = "a" -- allow the mouse to be used in neovim
---vim.opt.pumheight = 10 -- pop up menu height
---vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
---vim.opt.showtabline = 2 -- always show tabs
 --vim.opt.smartcase = true -- smart case
---vim.opt.splitbelow = true -- force all horizontal splits to go below current window
---vim.opt.splitright = true -- force all vertical splits to go to the right of current window
---vim.opt.swapfile = false -- creates a swapfile
 --vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
---vim.opt.title = true -- set the title of window to the value of the titlestring
---vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
---vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
---vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time
---vim.opt.wrap = false -- display lines as one long line
---vim.opt.scrolloff = 8 -- is one of my fav
---vim.opt.sidescrolloff = 8
 
 
+vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.wrap = true -- display lines as one long line
 
+vim.opt.backup = true -- creates a backup file
+vim.opt.writebackup = true -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
+
+local backupdir = vim.fn.stdpath("data") .. "/backups//"
+vim.opt.backupdir =  backupdir
+if vim.fn.finddir(backupdir) == "" then
+ vim.fn.mkdir(backupdir)
+end
+vim.opt.backupext = ".bak"
+vim.opt.backupcopy="yes"
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.undodir = "~/.local/share/nvim/undo//"
+local undodir = vim.fn.stdpath("data") .. "/undo//"
+vim.opt.undodir =  undodir
+if vim.fn.finddir(undodir) == "" then
+ vim.fn.mkdir(undodir)
+end
+
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 2 -- always show tabs
+
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.title = true -- set the title of window to the value of the titlestring
+vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.scrolloff = 8 -- is one of my fav
+vim.opt.sidescrolloff = 8
 vim.opt.updatetime = 300 -- faster completion
 vim.opt.undofile = true -- enable persistent undo
-vim.opt.undodir = vim.fn.stdpath "cache" .. "/undo"
+
+vim.opt.hidden = true -- required to keep multiple buffers and open multiple buffers
 
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 vim.opt.autoindent = true
