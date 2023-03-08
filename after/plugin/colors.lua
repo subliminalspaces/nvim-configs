2a57d8dc2604cadce0ac688ca5bbe74892b2b04d
abhishek mallepalli <mallepalliabhishek@gmail.com>
2023-03-07 22:13:38 -0800

    this function reset the background to transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,9 +4,9 @@ function ColorMyPencils(color)
         color = color or "tokyonight"
         vim.cmd.colorscheme(color)
         
-        --vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
- --       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
- --       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+        vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
 
---ColorMyPencils()
+ColorMyPencils()
