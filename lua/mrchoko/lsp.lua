4039d44146cbc64c83e31c7af29d7ac138023ee5
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2023-03-21 16:46:06 -0700

    replaced plugin for blankline indents and guides, onedark now allows for transparent backgrounds, and lua lsp no longer pesters about third party libs

--- lua/mrchoko/lsp.lua
+++ lua/mrchoko/lsp.lua
@@ -1,7 +1,3 @@
-
-
-
-
 require("lspconfig").html.setup {}
 require("lspconfig").tsserver.setup {}
 require("lspconfig").bashls.setup {}
@@ -9,9 +5,31 @@ require("lspconfig").eslint.setup {}
 require("lspconfig").graphql.setup {}
 
 require("lspconfig").sqlls.setup {
-single_file_support = false
+    single_file_support = false
 }
 require("lspconfig").quick_lint_js.setup {}
 require("lspconfig").jsonls.setup {}
-require("lspconfig").lua_ls.setup {}
+require("lspconfig").lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false, -- disable annoying 'do you need to configure your work enviroment as ... prompt'
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    }
+}
 require("lspconfig").yamlls.setup {}
---
+++ lua/mrchoko/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space:⋅"
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    opts={
+        show_current_context = true,
+        show_current_context_start = true,
+        show_end_of_line = true,
+        space_char_blankline = " "
+    }
+}
