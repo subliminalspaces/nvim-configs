5113d5cd4f89dfb5c86a228c5a2c5d04ed6818f9
abhishek mallepalli <mallepalliabhishek@gmail.com>
2023-03-21 21:09:33 -0700

    added some configs to fix sql and graphql server attachment

--- lua/mrchoko/lsp.lua
+++ lua/mrchoko/lsp.lua
@@ -2,10 +2,19 @@ require("lspconfig").html.setup {}
 require("lspconfig").tsserver.setup {}
 require("lspconfig").bashls.setup {}
 require("lspconfig").eslint.setup {}
-require("lspconfig").graphql.setup {}
+require("lspconfig").graphql.setup {
+    cmd = {
+        "graphql-lsp", "server", "-m", "stream"
+    },
+    filetypes = { "graphql", "typescriptreact", "javascriptreact" }
+    ,root_dir = require('lspconfig').util.root_pattern('.git','.graphqlrc*','.graphql.config.*','graphql.config.*')
+}
 
 require("lspconfig").sqlls.setup {
-    single_file_support = false
+    single_file_support = true,
+        cmd = {'sql-language-server', 'up', '--method', 'stdio'}   
+,filetypes = {'sql', 'mysql'},
+root_dir = require('lspconfig').util.root_pattern('.git')
 }
 require("lspconfig").quick_lint_js.setup {}
 require("lspconfig").jsonls.setup {}
