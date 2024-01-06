a897f64480b8136f26a45e8d9d0e6b769d19078e
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2024-01-05 17:34:07 -0800

    progress

--- lua/mrchoko/lsp.lua
+++ lua/mrchoko/lsp.lua
@@ -138,40 +138,9 @@ require('mason-lspconfig').setup({
     }
 })
 
--- local opts = {
---     tools = {
---         runnables = {
---             use_telescope = true,
---         },
---         inlay_hints = {
---             auto = true,
---             show_parameter_hints = false,
---             parameter_hints_prefix = "",
---             other_hints_prefix = "",
---         },
---     },
---     -- all the opts to send to nvim-lspconfig
---     -- these override the defaults set by rust-tools.nvim
---     -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
---     server = {
---         -- on_attach is a callback called when the language server attachs to the buffer
---         -- on_attach = on_attach,
---         settings = {
---             -- to enable rust-analyzer settings visit:
---             -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
---             ["rust-analyzer"] = {
---                 -- enable clippy on save
---                 checkOnSave = {
---                     command = "clippy",
---                 },
---             },
---         },
---     }
--- }
 local cmp = require('cmp')
 require("luasnip.loaders.from_vscode").lazy_load()
 local lspkind = require('lspkind')
--- require("rust-tools").setup(opts)
 cmp.setup({
     mapping =
     {
--- lua/mrchoko/plugins/ollama.lua
+++ lua/mrchoko/plugins/ollama.lua
@@ -1,3 +1,56 @@
+local curl = require "plenary.curl"
+-- local async = require"plenary.async"
+local split_output = {
+
+    fn = function()
+        return function(
+        -- model, context, prompt
+            body, job
+        )
+            -- prepare the command string
+            local filename = vim.fn.strftime("%d%m%y%H%M%S")
+            vim.cmd('vsplit')
+            local win = vim.api.nvim_get_current_win()
+            local buf = vim.api.nvim_create_buf(false, false)
+            vim.api.nvim_win_set_buf(win, buf)
+            vim.cmd("write " .. filename .. "<CR>")
+            -- local cmd = ("ollama run $model $prompt"):gsub("$model", model):gsub("$prompt",
+            --     vim.fn.shellescape(context .. "\n" .. prompt))
+            --
+            -- vim.fn.append(0, { "Sending '" .. prompt .. "' to model " .. model .. "." })
+            -- local response = curl.post("http://localhost:11434/api/generate", {
+            --     body = vim.fn.json_encode({
+            --         model = model,
+            --         prompt = prompt,
+            --         stream = false
+            --     })
+            -- })
+            -- local resjson = vim.json.decode(body.response)
+            -- local responsebody = vim.split(response.body, " ")
+            -- vim.uv.fs_write(filename)
+            local responseblocks = vim.split(body.response, "\n")
+            vim.fn.append(vim.fn.line('$'), responseblocks)
+
+            -- local line = vim.tbl_count(header) + 1
+            -- local words = {}
+            --
+            --    -- start the async job
+            -- return vim.fn.jobstart(cmd, {
+            -- 	on_stdout = function(_, data, _)
+            -- 		for i, token in ipairs(data) do
+            -- 			if i > 1 then -- if returned data array has more than one element, a line break occured.
+            -- 				line = line + 1
+            -- 				words = {}
+            -- 			end
+            -- 			table.insert(words, token)
+            -- 			vim.api.nvim_buf_set_lines(buf_nr, line, line + 1, false, {table.concat(words, "")})
+            -- 		end
+            -- 	end,
+            -- })
+        end
+    end
+}
+
 return {
     "nomnivore/ollama.nvim",
     dependencies = {
@@ -42,19 +95,25 @@ return {
                 model = "codellama",
                 action = "display",
             },
+            Roman_Empire = {
+                prompt = "Tell me a random fun fact about the Roman Empire\n",
+                model = "wizard-math:latest",
+                action = split_output
+            },
             Generate_Commit_Message = {
-                prompt = 'Write a short commit message according to the Conventional Commits specification for the following git diff: \n$buf',
+                prompt =
+                'Write a short commit message according to the Conventional Commits specification for the following git diff: \n$buf',
                 action = {
-                   fn = function()
-                    vim.cmd(":vnew")
-                    vim.cmd(":%!git diff --staged")
-                    return function(body, job)
-                        vim.cmd(":1,$d") -- wipe buffer
-                        -- vim.builtin.append(vim.builtin.line("."), tostring(body))
-                        vim.cmd("")
+                    fn = function()
+                        vim.cmd(":vnew")
+                        vim.cmd(":%!git diff --staged")
+                        return function(body, job)
+                            vim.cmd(":1,$d") -- wipe buffer
+                            -- vim.builtin.append(vim.builtin.line("."), tostring(body))
+                            vim.cmd("")
+                        end
                     end
-                end
-            }
+                }
 
             }
             -- Simplify_Code = {} --remove default prompt
